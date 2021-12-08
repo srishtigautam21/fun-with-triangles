46289e1528dde192c24eb562f6adde730e703807
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2021-12-08 21:19:50 +0530

    SumofangleofTriangle

--- main.js
+++ main.js
@@ -5,12 +5,19 @@ var outputE1=document.querySelector(".output");
 function calculateAngleSum(angle1,angle2,angle3)
 {
     var sumOfAngle=angle1+angle2+angle3;
+    return sumOfAngle;
 }
 
 function isAngleTriangle()
 {
-    var angle=inputAngle[0].value; //how to select individual rows with query selectorall
-    
-}
+    var sumOfAngle=calculateAngleSum(Number(inputAngle[0].value),Number(inputAngle[1].value),Number(inputAngle[2].value)); //how to select individual rows with query selectorall
+    if(sumOfAngle===180)
+    {
+        outputE1.innerText="Yay the angles form a triangle";
+    }
+    else{
+        outputE1.innerText="The angles do not form a triangle";
+    }
+}   
 
 btntriangle.addEventListener("click",isAngleTriangle);
